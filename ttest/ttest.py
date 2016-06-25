17805d414e5b73a0975115d8bc579dad1b6bf6fc
Christian Miller <christian.david.miller@gmail.com>
2016-06-24 22:11:16 -0700

    oops, remove print statements

--- ttest/ttest.py
+++ ttest/ttest.py
@@ -5,8 +5,6 @@ def one_sample_ttest(x, y):
     y = single value
     return = t_value as float"""
     mean_x = sum(x)/float(len(x))
-    print(mean_x)
     variance_x = sum(([(i - mean_x)**2 for i in x]))/float(len(x) - 1)
-    print(variance_x)
     t_value = (mean_x - y) / float((variance_x**(0.5)/ len(x)**(0.5)))
     return t_value
